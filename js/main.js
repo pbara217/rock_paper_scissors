bf59af4fbd82bd171abd804f2c0d22b909b77ab2
Peter Baran <piotrbaran@hotmail.com>
2023-09-10 14:48:50 -0600

    added player collection prompt

--- js/main.js
+++ js/main.js
@@ -4,5 +4,12 @@ function computerSelection (){
     console.log("The Computer chose: " + choices[randomChoice])
 
 }
-
+function playerSelection(){
+    const choices = [rock, paper, scissors]
+    let playerChoice = prompt("Let's play a game. Please choose one of the following: Rock, Paper, Scissors");
+    playerChoice = playerChoice.toLowerCase();
+    console.log(playerChoice)
+    
+}
+playerSelection()
 computerSelection()
