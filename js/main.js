cb219d9f6b6c03ef91fd3f623ba40a40570b1d50
Peter Baran <piotrbaran@hotmail.com>
2023-09-10 14:32:00 -0600

    created function getComputerChoice

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("hello world")
+function getComputerChoice (){
+    const choices = ["Rock", "Paper", "Scissors"];
+    let randomChoice = Math.floor(Math.random()*3)
+    console.log(randomChoice)
+    console.log(choices[randomChoice])
+
+}
+getComputerChoice()
