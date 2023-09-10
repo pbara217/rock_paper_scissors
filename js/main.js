dd474f1427e530f3a5bc2e45f6584d3554b4f4e5
Peter Baran <piotrbaran@hotmail.com>
2023-09-10 14:33:25 -0600

    Added text to getComputerchoice

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 function getComputerChoice (){
     const choices = ["Rock", "Paper", "Scissors"];
     let randomChoice = Math.floor(Math.random()*3)
-    console.log(randomChoice)
-    console.log(choices[randomChoice])
+    console.log("The Computer chose: " + choices[randomChoice])
 
 }
 getComputerChoice()
