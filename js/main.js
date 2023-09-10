81a31cadc4431d1897a264098afebd1797f056ad
Peter Baran <piotrbaran@hotmail.com>
2023-09-10 17:00:19 -0600

    reformated winner messages

--- js/main.js
+++ js/main.js
@@ -61,12 +61,6 @@ function game(){
         console.log("The current score is: You: " + playerWins + " Computer: " + computerWins)
     }
     console.log("The final score is: You: " + playerWins + " Computer: " + computerWins)
-    if ( playerWins > computerWins){
-        console.log("You Won!")
-    } if (playerWins < computerWins){
-        console.log("You Lost!")
-    } if (playerWins == computerWins){
-        console.log("It's a Tie!")
     } if (playerWins == 5){
         console.log("TOTAL DOMINATION!")
     } if (computerWins == 5){
@@ -74,6 +68,13 @@ function game(){
     } if (playerWins == 0 && computerWins == 0) {
         console.log("STALEMATE")
     }
+    if ( playerWins > computerWins){
+        console.log("You Won!")
+    } if (playerWins < computerWins){
+        console.log("You Lost!")
+    } if (playerWins == computerWins){
+        console.log("It's a Tie!")
+    
 }
 
 game()
