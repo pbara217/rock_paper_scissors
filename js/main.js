5fbfbc279695b93f7e0348ae45b750ec0a7b95b5
Peter Baran <piotrbaran@hotmail.com>
2023-09-10 16:04:01 -0600

    Added Tie results

--- js/main.js
+++ js/main.js
@@ -18,3 +18,17 @@ function getplayerSelection(){
 const computerSelection = getcomputerSelection();
 const playerSelection = getplayerSelection();
 console.log(playerSelection + " " + computerSelection)
+function playRound(playerSelection, computerSelection){
+    let player = playerSelection;
+    let computer = computerSelection;
+    let result = " ";
+    if (player == "rock" && computer == "rock"){
+        return "You have picked Rock. The Computer has picked Rock.\n This is a tie."
+    } if (player == "paper" && computer == "paper"){
+        return "You have picked Paper. The Computer has picked Paper.\n This is a tie."
+    } if (player == "scissors" && computer == "scissors"){
+        return "You have picked Scissors. The Computer has picked Scissors.\n This is a tie."
+    }
+
+}
+console.log(playRound(playerSelection, computerSelection))
