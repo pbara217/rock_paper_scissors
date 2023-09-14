0870e50a5cfba6779bf61cd3211fd92a50d62448
Peter Baran <piotrbaran@hotmail.com>
2023-09-14 17:45:54 -0600

    Cleaned up misc

--- js/main.js
+++ js/main.js
@@ -76,36 +76,4 @@ function getplayerSelection(){
    return playerChoice;
 }
 
-
-function playRound(playerSelection, computerSelection){
-    let player = playerSelection;
-    let computer = computerSelection;
-    let result = " ";
-    if (player == "rock" && computer == "rock"){
-        return "You have picked Rock. The Computer has picked Rock.\n This is a tie."
-    } if (player == "rock" && computer == "paper"){
-        return "You Lose! Paper covers Rock"
-    } if (player == "rock" && computer == "scissors"){
-        return "You Win! Rock smashes Scissors"
-    } 
-    if (player == "paper" && computer == "paper"){
-        return "You have picked Paper. The Computer has picked Paper.\n This is a tie."
-    } if (player == "paper" && computer == "rock"){
-        return "You Win! Paper covers Rock"
-    } if (player == "paper" && computer == "scissors"){
-        return "You Lose! Scissors cuts Paper"
-    }
-     if (player == "scissors" && computer == "scissors"){
-        return "You have picked Scissors. The Computer has picked Scissors.\n This is a tie."
-    } if (player == "scissors" && computer == "paper"){
-        return "You Win! Scissors cuts Paper" 
-    } if (player == "scissors" && computer == "rock"){
-        return "You Lose! Rock smashes scissors"
-    } 
-
-
-
-}
-
 getplayerSelection()
-
